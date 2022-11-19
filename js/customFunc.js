79427c1ff4710c057c581f3f017d90c669d2236b
vipin017 <vpnchndr017@gmail.com>
2022-11-19 15:33:58 +0530

    Popup added

--- js/customFunc.js
+++ js/customFunc.js
@@ -13,6 +13,28 @@ $(function() {
   });
 });
 
+function PopUp(hideOrshow) {
+    if (hideOrshow == 'hide') document.getElementById('ac-wrapper').style.display = "none";
+    else document.getElementById('ac-wrapper').removeAttribute('style');
+}
+window.onload = function () {
+    setTimeout(function () {
+        PopUp('show');
+    }, 3000);
+}
+
+// <script type="text/javascript">
+// function PopUp(hideOrshow) {
+//     if (hideOrshow == 'hide') document.getElementById('ac-wrapper').style.display = "none";
+//     else document.getElementById('ac-wrapper').removeAttribute('style');
+// }
+// window.onload = function () {
+//     setTimeout(function () {
+//         PopUp('show');
+//     }, 5000);
+// }
+// </script>
+
 // $(document).ready(function() {
 //   $(".hem-btn").click(function() {
 //     $(".side-menu").addClass("open");
