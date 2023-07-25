7508338a3cfc002f693d405c5c137b2cd34c4798
vipin017 <vpnchndr017@gmail.com>
2023-07-25 18:56:37 +0530

    changes

--- js/customFunc.js
+++ js/customFunc.js
@@ -13,16 +13,6 @@ $(function() {
   });
 });
 
-function PopUp(hideOrshow) {
-    if (hideOrshow == 'hide') document.getElementById('ac-wrapper').style.display = "none";
-    else document.getElementById('ac-wrapper').removeAttribute('style');
-}
-window.onload = function () {
-    setTimeout(function () {
-        PopUp('show');
-    }, 800);
-}
-
 // <script type="text/javascript">
 // function PopUp(hideOrshow) {
 //     if (hideOrshow == 'hide') document.getElementById('ac-wrapper').style.display = "none";
